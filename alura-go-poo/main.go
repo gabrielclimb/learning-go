f2503e3e31f9ac94ec6d68c212190691e1292f00
Gabriel Soares <gabriel.soares@saltpay.co>
2022-06-23 20:53:03 -0300

    end of second part, intro to oop

--- alura-go-poo/main.go
+++ alura-go-poo/main.go
@@ -13,14 +13,26 @@ func NewContaCorrente() *ContaCorrente {
 	return &ContaCorrente{}
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
 
-	contaGabriel := ContaCorrente{
-		titular:       "Gabriel",
-		numeroAgencia: 2656,
-		numeroConta:   5458,
-		saldo:         25898.56,
-	}
+	contaGabriel := NewContaCorrente()
+	contaGabriel.titular = "Gabriel"
+	contaGabriel.numeroAgencia = 546
+	contaGabriel.numeroConta = 123
+	contaGabriel.saldo = 5465.58
 
-	fmt.Println(contaGabriel)
+	fmt.Println(contaGabriel.saldo)
+
+	fmt.Println(contaGabriel.Sacar(600))
+	fmt.Println(contaGabriel.saldo)
 }
