ed22a69ccc870ef05cadeb9123fb8004e4618bcf
Gabriel Soares <gabriel.soares@saltpay.co>
2022-06-24 17:03:45 -0300

    fim do quarto modulo

--- alura-go-poo/main.go
+++ alura-go-poo/main.go
@@ -2,24 +2,38 @@ package main
 
 import (
 	"fmt"
+	"poo/clientes"
 	"poo/contas"
 )
 
 func main() {
 
-	contaGabriel := contas.ContaCorrente{
-		Titular:       "Gabriel Soares",
-		NumeroAgencia: 1234,
-		NumeroConta:   5647,
-		Saldo:         200,
+	clienteGabriel := clientes.Titular{
+		Nome:      "Gabriel Soares",
+		CPF:       "31485698533",
+		Profissao: "Engenheiro de Software",
 	}
 
-	contaRenata := contas.ContaCorrente{
-		Titular:       "Renata Panseri",
-		NumeroAgencia: 556,
-		NumeroConta:   489,
-		Saldo:         300,
+	contaGabriel := contas.ContaCorrente{
+		Titular:       clienteGabriel,
+		NumeroAgencia: 1234,
+		NumeroConta:   5647,
 	}
+
+	contaGabriel.Depositar(200)
+	fmt.Println(contaGabriel)
+	fmt.Println("Saldo => ", contaGabriel.MostraSaldo())
+	//contaRenata := contas.ContaCorrente{
+	//	Titular: clientes.Titular{
+	//		Nome:      "Renata Panseri",
+	//		CPF:       "12345678900",
+	//		Profissao: "Manda Chuva",
+	//	},
+	//	NumeroAgencia: 556,
+	//	NumeroConta:   489,
+	//	Saldo:         300,
+	//}
+	//fmt.Println(contaRenata)
 
 	//fmt.Println(contaGabriel.Saldo)
 	//fmt.Println(contaGabriel.Sacar(600))
@@ -27,10 +41,10 @@ func main() {
 	//status, valor := contaGabriel.Depositar(654.56)
 	//fmt.Println(status, "\nSaldo: $", valor)
 
-	status := contaRenata.Transferir(300, &contaGabriel)
-
-	fmt.Println(status)
-	fmt.Println("Saldo Gabriel:", contaGabriel.Saldo)
-	fmt.Println("Saldo Renata:", contaRenata.Saldo)
+	//status := contaRenata.Transferir(300, &contaGabriel)
+	//
+	//fmt.Println(status)
+	//fmt.Println("Saldo Gabriel:", contaGabriel.Saldo)
+	//fmt.Println("Saldo Renata:", contaRenata.Saldo)
 
 }
