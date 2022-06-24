8a66d3fa0e8083f4e491509cfdb7ac965bba1000
Gabriel Soares <gabriel.soares@saltpay.co>
2022-06-24 16:36:17 -0300

    create contas module

--- alura-go-poo/main.go
+++ alura-go-poo/main.go
@@ -1,67 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"contas/contas"
+	"fmt"
+)
 
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Depósito realizado com sucesso", c.saldo
-	}
-	return "Valor de depósito inválido", c.saldo
-
-}
-
-func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia <= c.saldo && valorDaTransferencia > 0 {
-		c.saldo -= valorDaTransferencia
-		contaDestino.Depositar(valorDaTransferencia)
-		return true
-	}
-	return false
-}
 func main() {
 
-	contaGabriel := ContaCorrente{
-		titular:       "Gabriel Soares",
-		numeroAgencia: 1234,
-		numeroConta:   5647,
-		saldo:         200,
+	contaGabriel := contas.ContaCorrente{
+		Titular:       "Gabriel Soares",
+		NumeroAgencia: 1234,
+		NumeroConta:   5647,
+		Saldo:         200,
 	}
 
-	contaRenata := ContaCorrente{
-		titular:       "Renata Panseri",
-		numeroAgencia: 556,
-		numeroConta:   489,
-		saldo:         300,
+	contaRenata := contas.ContaCorrente{
+		Titular:       "Renata Panseri",
+		NumeroAgencia: 556,
+		NumeroConta:   489,
+		Saldo:         300,
 	}
 
-	//fmt.Println(contaGabriel.saldo)
+	//fmt.Println(contaGabriel.Saldo)
 	//fmt.Println(contaGabriel.Sacar(600))
-	//fmt.Println(contaGabriel.saldo)
+	//fmt.Println(contaGabriel.Saldo)
 	//status, valor := contaGabriel.Depositar(654.56)
 	//fmt.Println(status, "\nSaldo: $", valor)
 
 	status := contaRenata.Transferir(300, &contaGabriel)
 
 	fmt.Println(status)
-	fmt.Println("Saldo Gabriel:", contaGabriel.saldo)
-	fmt.Println("Saldo Renata:", contaRenata.saldo)
+	fmt.Println("Saldo Gabriel:", contaGabriel.Saldo)
+	fmt.Println("Saldo Renata:", contaRenata.Saldo)
 
 }
