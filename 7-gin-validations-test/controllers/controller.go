4cc95bcdbad1d5225e19df27c2aae3adb56f3329
Gabriel Barbosa Soares <gabriel.soares@saltpay.co>
2023-07-09 01:44:01 +0100

    starting

---
+++ 7-gin-validations-test/controllers/controller.go
@@ -0,0 +1,83 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/guilhermeonrails/api-go-gin/database"
+	"github.com/guilhermeonrails/api-go-gin/models"
+)
+
+func ShowAllStudents(c *gin.Context) {
+	var students []models.Student
+	database.DB.Find(&students)
+	c.JSON(200, students)
+}
+
+func Saudacao(c *gin.Context) {
+	nome := c.Params.ByName("nome")
+	c.JSON(200, gin.H{
+		"API diz:": "E ai " + nome + ", tudo beleza?",
+	})
+}
+
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Student
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
+}
+
+func BuscaAlunoPorID(c *gin.Context) {
+	var aluno models.Student
+	id := c.Params.ByName("id")
+	database.DB.First(&aluno, id)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Student não encontrado"})
+		return
+	}
+
+	c.JSON(http.StatusOK, aluno)
+}
+
+func DeletaAluno(c *gin.Context) {
+	var aluno models.Student
+	id := c.Params.ByName("id")
+	database.DB.Delete(&aluno, id)
+	c.JSON(http.StatusOK, gin.H{"data": "Student deletado com sucesso"})
+}
+
+func EditaAluno(c *gin.Context) {
+	var aluno models.Student
+	id := c.Params.ByName("id")
+	database.DB.First(&aluno, id)
+
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	database.DB.Model(&aluno).UpdateColumns(aluno)
+	c.JSON(http.StatusOK, aluno)
+}
+
+func BuscaAlunoPorCPF(c *gin.Context) {
+	var aluno models.Student
+	cpf := c.Param("cpf")
+	database.DB.Where(&models.Student{CPF: cpf}).First(&aluno)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Student não encontrado"})
+		return
+	}
+
+	c.JSON(http.StatusOK, aluno)
+}
